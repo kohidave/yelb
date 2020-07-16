4c99589ac4f11d41042e3c61f47f8a7340c62291
David Killmon <killmond+gh@amazon.com>
2020-07-16 15:17:07 -0700

    yelb

--- yelb-ui/startup.sh
+++ yelb-ui/startup.sh
@@ -4,18 +4,18 @@ cd clarity-seed
 
 # when the variable is populated a search domain entry is added to resolv.conf at startup
 # this is needed for the ECS service discovery given the app works by calling host names and not FQDNs
-# a search domain can't be added to the container when using the awsvpc mode 
-# and the awsvpc mode is needed for A records (bridge only supports SRV records) 
-if [ $SEARCH_DOMAIN ]; then echo "search ${SEARCH_DOMAIN}" >> /etc/resolv.conf; fi 
+# a search domain can't be added to the container when using the awsvpc mode
+# and the awsvpc mode is needed for A records (bridge only supports SRV records)
+if [ $SEARCH_DOMAIN ]; then echo "search ${SEARCH_DOMAIN}" >> /etc/resolv.conf; fi
 
 sed -i -- 's#/usr/share/nginx/html#/clarity-seed/'$UI_ENV'/dist#g' $NGINX_CONF
 
-# this adds the reverse proxy configuration to nginx 
-# everything that hits /api is proxied to the app server     
+# this adds the reverse proxy configuration to nginx
+# everything that hits /api is proxied to the app server
 if ! grep -q "location /api" "$NGINX_CONF"; then
     eval "cat <<EOF
     location /api {
-        proxy_pass http://yelb-appserver:4567/api;
+        proxy_pass http://appserver.yelb.local:4567/api;
         proxy_http_version 1.1;
     }
     gzip on;
@@ -27,6 +27,6 @@ EOF
     sed --in-place '/server_name  localhost;/ r /proxycfg.txt' $NGINX_CONF
 fi
 
-nginx -g "daemon off;" 
+nginx -g "daemon off;"
 
 
